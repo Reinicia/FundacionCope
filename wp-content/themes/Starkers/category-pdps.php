c0645314fa4b521fa952effe1b7b9c1ab29e0160
ccrespo <ccrespo@todovino.com>
2012-10-23 22:57:13 +0200

    Fix los profes

--- wp-content/themes/Starkers/category-pdps.php
+++ wp-content/themes/Starkers/category-pdps.php
@@ -25,7 +25,7 @@ get_header(); ?>
 
 // default loop here, if applicable, followed by wp_reset_query();
         $args = array(
-	            'category_name'=>'profesores',
+	            'category_name'=>'pdp\'s',
                 'posts_per_page' => 9,
 	            'paged' => $my_page
         );
